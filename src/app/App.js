2c202962f0495d2d2a26c819186e680af8695a6d
Dazai <proyectpag@gmail.com>
2019-07-08 17:17:12 -0500

    Ya quedo terminada la App, solo queda mejorarla

--- src/app/App.js
+++ src/app/App.js
@@ -5,28 +5,99 @@ class App extends Component{
         super();
         this.state = {
             title: '',
-            description: ''
+            description: '',
+            tasks: [],
+            _id: ''
         };
         this.addTask = this.addTask.bind(this);
         this.handleChange = this.handleChange.bind(this);
     }
 
-    addTask(e){
-        // console.log(this.state);
-        fetch('/api/tareas', {
-            method: 'POST',
-            body: JSON.stringify(this.state),
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            }
-        })
-        // .then(res => console.log(res))
-        .then(res => res.json())
-        .then(data => console.log(data))
-        .catch(err => console.log(err))
+    addTask(e){//Agrega Tareas
+        if (this.state._id) {
+            fetch(`/api/tareas/${this.state._id}`, {
+                method: 'PUT',
+                body: JSON.stringify(this.state),
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                }
+            })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                M.toast({html: 'Task Update'});
+                this.setState({title:'', description: '', _id: ''});
+                this.fetchTask();
+            });
+        } else {
+            // console.log(this.state);
+            fetch('/api/tareas', {
+                method: 'POST',
+                body: JSON.stringify(this.state),
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                }
+            })
+            // .then(res => console.log(res))
+            .then(res => res.json())
+            // .then(data => console.log(data))
+            .then(data =>{
+                console.log(data);
+                M.toast({html: 'Task Save!'});
+                this.setState({title:'', description:''});
+                this.fetchTask();
+            })
+            .catch(err => console.log(err));
+        }
 
         e.preventDefault();//Evita que se recarge al pagina al enviar el formulario
+    }
+
+    deleteTask(id){
+        if (confirm('Are you sure you want to delete task?')) {
+            // console.log('deleting...', id);
+            fetch(`/api/tareas/${id}`, {
+                method: 'DELETE',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                }
+            })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                M.toast({html: 'Task Delete'});
+                this.fetchTask();
+            });
+        }
+    }
+
+    editTask(id){
+        fetch(`/api/tareas/${id}`)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            this.setState({
+                title: data.title,
+                description: data.description,
+                _id: data._id
+            })
+        });
+    }
+
+    componentDidMount(){//Activa la funcion inmediatamente despues de cargar/iniciar
+        this.fetchTask();
+    }
+    fetchTask(){//Obtener Tareas
+        fetch('/api/tareas')
+            .then(res => res.json())
+            .then(data => {
+                // console.log(data);
+                this.setState({tasks: data});
+                console.log(this.state.tasks);
+            });
     }
 
     handleChange(e){
@@ -54,12 +125,12 @@ class App extends Component{
                                     <form onSubmit={this.addTask}>
                                         <div className="row">
                                             <div className="input-field col s12">
-                                                <input name="title" onChange={this.handleChange} type="text" placeholder="Task Title" />
+                                                <input name="title" onChange={this.handleChange} type="text" placeholder="Task Title" value={this.state.title}/>
                                             </div>
                                         </div>
                                         <div className="row">
                                             <div className="input-field col s12">
-                                                <textarea name="description" onChange={this.handleChange} placeholder="Task Description" className="materialize-textarea"></textarea>
+                                                <textarea name="description" onChange={this.handleChange} placeholder="Task Description" className="materialize-textarea" value={this.state.description}></textarea>
                                             </div>
                                         </div>
                                         <button type="submit" className="btn light-blue darken-4">
@@ -70,7 +141,34 @@ class App extends Component{
                             </div>
                         </div>
                         <div className="col s7">
-
+                            <table>
+                                <thead>
+                                    <tr>
+                                        <th>Title</th>
+                                        <th>Description</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {
+                                        this.state.tasks.map(task => {
+                                            return (
+                                                <tr key={task._id}>
+                                                    <td>{task.title}</td>
+                                                    <td>{task.description}</td>
+                                                    <td>
+                                                        <button className="btn light-blue darken-4" onClick={() => this.editTask(task._id)}>
+                                                            <i className="material-icons">edit</i>
+                                                        </button>
+                                                        <button className="btn light-blue darken-4" onClick={() => this.deleteTask(task._id)}>
+                                                            <i className="material-icons">delete</i>
+                                                        </button>
+                                                    </td>
+                                                </tr>
+                                            )
+                                        })
+                                    }
+                                </tbody>
+                            </table>
                         </div>
                     </div>
                 </div>
